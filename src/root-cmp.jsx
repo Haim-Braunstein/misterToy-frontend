a86961ba0c43ad3e5fb1bc85b18540cd947bfee2
Haim Braunstein <haim321@gmail.com>
2024-03-26 15:39:11 +0200

    Init repo

---
+++ src/root-cmp.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../src/assets/style/main.css'
+
+export function App() {
+
+  return (
+    <h1>Hello</h1>
+  )
+
+}
+
