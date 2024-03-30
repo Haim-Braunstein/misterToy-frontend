aff8ba20c38c5c2869dc48ca8c5579094684cd12
Haim Braunstein <haim321@gmail.com>
2024-03-30 14:46:01 +0300

    Add bg img to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
 
-
 export function HomePage() {
 
-}
+    return (
+        <section className="img-container"> 
+            <img className="home-img" src="img/toy-bg.jpg" />
+        </section>
+
+)
+    }
