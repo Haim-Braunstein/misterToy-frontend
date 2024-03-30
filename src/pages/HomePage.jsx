c0fdbced99a99a7f017385030a0093ef0e857b35
Haim Braunstein <haim321@gmail.com>
2024-03-30 21:26:17 +0300

    Add welcome words in the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,16 @@
 export function HomePage() {
 
     return (
-        <section className="img-container"> 
+        <section className="img-container">
             <img className="home-img" src="img/toy-bg.jpg" />
+            <div className="welcome-words">
+            <h1>Welcome to Toy Kingdom</h1>
+            <p>Toy Kingdom is your one-stop destination for all things toys! With a wide selection
+                of toys for children of all ages, we strive to bring joy and excitement to families
+                everywhere. Whether you're looking for the latest action figures,
+                educational toys, or classic board games, you'll find it all at Toy Kingdom.</p>
+                </div>
         </section>
 
-)
-    }
+    )
+}
