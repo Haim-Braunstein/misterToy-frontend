249c772e739e574364aae9d123e63c2070032c28
Haim Braunstein <haim321@gmail.com>
2024-04-01 22:40:06 +0300

    Fix media query in toy filter

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../misterToy-backend/public',
+    outDir: '../misterToy-new-backend/public',
     emptyOutDir: true,
   }
 })
